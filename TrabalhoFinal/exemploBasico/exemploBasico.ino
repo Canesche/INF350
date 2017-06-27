c9da22a9ee81ff2d434e6296d1120b5db8553437
canesche <michael.canesche@gmail.com>
2017-06-27 07:50:44 -0300

    adicao de exemplo simples que funciona o HC-12

---
+++ TrabalhoFinal/exemploBasico/exemploBasico.ino
@@ -0,0 +1,29 @@
+/**
+ * Exemplo básico para HC-12
+ */
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3); //RX, TX
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
+}
+
+void loop() {
+  
+  if(Serial.available() > 0){ // ler a partir do serial monitor envia para o HC-12
+    String input = Serial.readString();
+    mySerial.println(input);
+    Serial.print("Voce enviou: ");
+    Serial.println(input);    
+  }
+ 
+  if(mySerial.available() > 1){ // ler a partir do HC-12 and envia para o serial monitor
+    
+    String input = mySerial.readString();
+    Serial.println(input);    
+  }
+  delay(20);
+}
